6ba8090e15206c9e7eea0f8278cc236e634f2570
Jamie <jamiebloomfield8@gmail.com>
2024-07-23 20:48:09 +0100

    Add icons, give #themeSwitch opposite theme
    Adds dependencies on jQuery & Bootstrap-icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -14,14 +14,11 @@ const getPreferredTheme = () => {
 }
 
 const setTheme = theme => {
-    if (theme === 'auto') {
-    document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
-    } else {
-    document.documentElement.setAttribute('data-bs-theme', theme)
-    }
-}
+    document.documentElement.setAttribute('data-bs-theme', theme);
 
-setTheme(getPreferredTheme())
+    let opposite = (theme == 'dark') ? 'light' : 'dark';
+    $("#themeSwitch").addClass('btn-' + opposite).removeClass('btn-' + theme);
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     setTheme(getPreferredTheme())
@@ -29,12 +26,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
 document.getElementById("themeSwitch").addEventListener("click", () => {
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-    setTheme("light")
-    setStoredTheme("light")
+        setTheme("light")
+        setStoredTheme("light")
     }
     else {
-    setTheme("dark")
-    setStoredTheme("dark")
+        setTheme("dark")
+        setStoredTheme("dark")
     }
 });
 
